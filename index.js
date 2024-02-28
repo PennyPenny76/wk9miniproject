50803dcd54c80c329b84ba4168f78b0e7b74308c
PennyPenny76 <pengyingtong@hotmail.com>
2024-02-28 13:11:11 +1030

    finished

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+
+const generateHTML = ({ name, location, github, linkedin }) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+<title>Document</title>
+</head>
+<body>
+<header class="p-5 mb-4 header bg-light">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${name}</h1>
+    <p class="lead">I am from ${location}.</p>
+    <h3>Example heading <span class="badge bg-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${github}</li>
+      <li class="list-group-item">LinkedIn: ${linkedin}</li>
+    </ul>
+  </div>
+</header>
+</body>
+</html>`;
 
 inquirer
   .prompt([
@@ -16,16 +41,20 @@ inquirer
     {
       type: "input",
       message: "what is your LinkenIn URL?",
-      name: "LinkenIn",
+      name: "linkedin",
     },
     {
         type: "input",
         message: "what is your GitHub URL?",
-        name: "GitHub",
+        name: "github",
       },
   ])
-  .then((response) =>
-    response.confirm
-      ? console.log("Success!")
-      : console.log("err! Please answer all the questions.")
-  );
+  .then((response) => {
+   
+    const filename = `${response.name.toLowerCase().split(" ").join("")}.html`;
+  
+
+    fs.writeFile(filename, generateHTML(response), (err) =>
+    err ? console.log(err) : console.log("Success!")
+    );
+});
